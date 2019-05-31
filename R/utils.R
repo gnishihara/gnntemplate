708875c2e2d3faf037bd7d5e1ac326e676e6137f
gnishihara <greg@nagasaki-u.ac.jp>
2019-05-31 10:32:20 +0900

    A general Beamer template.

---
+++ R/utils.R
@@ -0,0 +1,21 @@
+# Borrowed from rticles to avoid having to deal w/ name space issues
+# Not exported
+
+
+find_file <- function(template, file) {
+  template <- system.file("rmarkdown", "templates",
+                          "gnn-beamer",file,
+                          package = "gnntemplate")
+  if (template == "") {
+    stop("Couldn't find template file ", template, "/", file, call. = FALSE)
+  }
+
+  template
+}
+
+find_resource <- function(template, file) {
+  find_file(template, file.path("resources", file))
+}
+
+
+find_resource("gnn_beamer", "template.tex")
